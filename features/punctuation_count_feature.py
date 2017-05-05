80071ed7d08e476bf535d07041c922c52bbae61c
fdzidic <filipdzidic@gmail.com>
2017-05-05 04:29:23 +0200

    Added punctuation count feature.

---
+++ features/punctuation_count_feature.py
@@ -0,0 +1,57 @@
+from feature import Feature
+
+import numpy as np
+import pickle
+import sys
+sys.path.insert(1, '../dataset')
+import dataset_reader as dr
+
+class PunctuationCountFeature(Feature):
+
+
+    def __init__(self):
+        self.map = {}
+        self.punctuation_mapper = {
+            "," : 0,
+            "." : 1,
+            ";" : 2,
+            "!" : 3,
+            "?" : 4,
+            "-" : 5
+        }
+        self.punc_marks = [',','.',';','!','?','-']
+
+
+    def extract_feature(self, user, tweets):
+        
+        tweet_count = len(tweets)
+        if tweet_count == 0:
+            self.map[user] = list(zip(self.punc_marks, np.zeros(6)))
+            return
+
+        total_counts = np.zeros(6)
+        for tweet in tweets:
+            counts = np.zeros(6)
+            char_count = 0
+            for word in tweet:
+                if(word != "NUMBER" or word != "URL"):
+                    char_count+=len(word)
+                    for char in word:
+                        if char in self.punc_marks:
+                            counts[self.punctuation_mapper[char]]+=1.0
+            if(char_count==0):
+                continue
+            counts/=char_count
+            total_counts+=counts
+
+        total_counts/=tweet_count
+        self.map[user] = dict(zip(self.punc_marks, total_counts))
+
+
+data = dr.load_dataset()
+PCF = PunctuationCountFeature()
+for user in data:
+    PCF.extract_feature(user, data[user].get_tweets())
+
+for user in PCF.map.keys():
+    print(user, PCF.map[user])
